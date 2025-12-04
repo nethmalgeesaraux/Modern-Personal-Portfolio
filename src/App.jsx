bbe33d4a158dd28e8331d4b876c79fdd5df86d4b
nethmalgeesaraux <nethmalpaypal@gmail.com>
2025-12-04 21:45:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { NotFound } from "./assets/pages/NotFound"
+import { Home } from "./assets/pages/Home"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home/>} />
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
